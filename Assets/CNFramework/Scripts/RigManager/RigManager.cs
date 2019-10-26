31dfb0970cf69e6037acdb126276deb07f14d5b2
marccrobbins <marccrobbins@gmail.com>
2019-10-25 21:49:41 -0400

    added position getters to rigmanager

--- Assets/CNFramework/Scripts/RigManager/RigManager.cs
+++ Assets/CNFramework/Scripts/RigManager/RigManager.cs
@@ -8,6 +8,19 @@ namespace CNFramework
     {
         public static RigManager Instance;
 
+        [SerializeField] private Transform head;
+        [SerializeField] private Transform leftHand;
+        [SerializeField] private Transform rightHand;
+
+        public Vector3 HeadPosition => head.position;
+        public Vector3 LocalHeadPosition => head.localPosition;
+        
+        public Vector3 LeftHandPosition => leftHand.position;
+        public Vector3 LocalLeftHandPosition => leftHand.localPosition;
+        
+        public Vector3 RightHandPosition => rightHand.position;
+        public Vector3 LocalRightHandPosition => rightHand.localPosition;
+
         private void Awake()
         {
             if (!Instance) Instance = this;
